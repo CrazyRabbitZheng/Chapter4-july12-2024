5e61bc7e685c616d3f40076a190397c7eb879bae
CrazyRabbitZheng <alicejrha@gmail.com>
2024-07-12 13:56:31 -0230

    update

--- ClassAverage.java
+++ ClassAverage.java
@@ -9,17 +9,25 @@ public class ClassAverage {
         Scanner in = new Scanner(System.in);
 
         int total = 0;
-        int gradeCounter = 1;
+        int gradeCounter = 0;
 
-        while(gradeCounter <= 10){
-            System.out.print("Enter grade of " + gradeCounter + ": ");
-            int grade = in.nextInt();
+        System.out.println("enter grade or enter -1 to quit");
+        int grade = in.nextInt();
+
+        while (grade != -1) {
             total += grade;
             gradeCounter++;
+            System.out.println("enter grade or enter -1 to quit");
+            grade = in.nextInt();
+
         }
 
-        int average = total / 10;
-        System.out.println("Class total grade is " + total);
-        System.out.println("Class average is " + average);
+        if(gradeCounter != 0) {
+            double average = (double)total / gradeCounter;//如果不用double cast，这里两个integer相除，小数部分cast，精确性不高。
+            System.out.println("Class total is " + total);
+            System.out.printf("Average is %.2f", average);
+        }else {
+            System.out.println("No grade in entered");
+        }
     }
 }
